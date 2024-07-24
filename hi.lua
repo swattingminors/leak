898e5870cc1bda8d56c68fc2a922f3165471a7b6
lunawaree <146039793+lunawaree@users.noreply.github.com>
2024-07-24 17:32:13 -0400

    Update hi.lua

--- hi.lua
+++ hi.lua
@@ -185,7 +185,6 @@ getgenv().ukiyo = {
 
 script_key = getgenv().ukiyo.Whitelist.LuaKey
 
-print("discord.gg/ukiyos / attacaksent")
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local Workspace = game:GetService("Workspace")
